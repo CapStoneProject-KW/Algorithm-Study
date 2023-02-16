5a0078daf061425d170d27bf24b320577c91857f
speardragon <rkdckdfyyd@naver.com>
2023-02-16 10:26:53 +0900

    [speardragon] 230216-bj_11057

---
+++ 230213/speardragon/bj_11057.py
@@ -0,0 +1,60 @@
+import sys
+sys.stdin = open("bj_11057_in.txt", "r")
+input = sys.stdin.readline
+'''
+idea: sort를 해도 그대로
+memo[] = 오르막 개수
+
+f(1)=10
+
+f(2)=55
+00 01 02 03 04 ~ 09
+11 12 13 14 
+22 23 24
+33
+44
+55
+...
+99
+
+f(3)
+000~999
+000 001 002 003
+011 012
+
+
+
+
+
+889 899
+999
+
+
+f(4)
+0000 0001
+
+'''
+
+
+def solution(memo):
+
+  tmp = memo
+
+  # for _ in range(N):
+  for i in range(10):
+      total = 0
+      for j in range(0, i+1):
+        total += tmp[j]
+      print(total)
+      memo[i] = total
+      print(memo,tmp)
+  tmp = memo
+  print(memo)
+      
+
+N = int(input())
+_memo = [0] * (1001)
+_memo = [i for i in range(1, 11)]
+_memo = [1 for i in range(10)]
+
+solution(_memo)
