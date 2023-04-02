23935e38d678f1e2d53dfafbff9a1ecf44f6825d
speardragon <rkdckdfyyd@naver.com>
2023-04-02 10:38:58 +0900

    [speardragon] bj_18222

---
+++ 230406/speardragon/bj_18222.py
@@ -0,0 +1,26 @@
+import sys
+sys.stdin = open("bj_18222_in.txt")
+input = sys.stdin.readline
+'''
+k=4
+0110
+
+f(1) = 0
+f(2) = 01
+f(3) = 0110
+f(4) = 01101001
+
+해당 위치가 어느 level에 위치해 있는지
+level-1 헀을 때 절반을 기준으로
+'''
+
+
+def solution(x, len):
+    
+
+        
+
+k = int(input())
+
+solution(k)
+
